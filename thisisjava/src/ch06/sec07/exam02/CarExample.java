794f2973c8f3ef5811af90c15d68f8cf14d9c54b
JaewonYooTime <dkrkvudghk@naver.com>
2025-02-12 14:55:26 +0900

    ~231

---
+++ thisisjava/src/ch06/sec07/exam02/CarExample.java
@@ -0,0 +1,11 @@
+package ch06.sec07.exam02;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car myCar = new Car("그랜저", "검정", 300);
+//		System.out.println(myCar.model); // 에러
+
+	}
+
+}
