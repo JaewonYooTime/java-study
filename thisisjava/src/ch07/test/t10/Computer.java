c5ca9c18e68479ed964e8ee4a8a72f17ac56dd8e
JaewonYooTime <dkrkvudghk@naver.com>
2025-05-04 03:52:19 +0900

    25.05.04

---
+++ thisisjava/src/ch07/test/t10/Computer.java
@@ -0,0 +1,7 @@
+package ch07.test.t10;
+
+public class Computer extends Machine {
+	public void work() {
+		System.out.println("추상클래스에서 상속받은 work() 메소드 오버라이딩 완료");
+	}
+}
---
+++ thisisjava/src/ch07/test/t11/MainActivityExample.java
@@ -0,0 +1,8 @@
+package ch07.test.t11;
+
+public class MainActivityExample {
+	public static void main(String[] args) {
+		MainActivity mainActivity = new MainActivity();
+		mainActivity.onCreate();
+	}
+}
