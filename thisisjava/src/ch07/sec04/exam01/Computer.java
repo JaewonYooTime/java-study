78d1e63f17e680db9f4898831141e2385b560a23
JaewonYooTime <dkrkvudghk@naver.com>
2025-05-03 23:34:24 +0900

    25.05.03

--- thisisjava/src/ch07/sec04/exam01/Computer.java
+++ thisisjava/src/ch07/sec04/exam01/Computer.java
@@ -1,7 +1,7 @@
 package ch07.sec04.exam01;
 
 public class Computer extends Calculator {
-	@Override
+//	@Override
 	public double areaCircle(double r) {
 		System.out.println("Computer 객체의 areaCircle() 실행");
 		return Math.PI * r * r;
