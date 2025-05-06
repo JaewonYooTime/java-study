2128b586b71e8534ebdd4fca8bf6f46bd7c22344
JaewonYooTime <dkrkvudghk@naver.com>
2025-05-07 07:09:01 +0900

    25.05.07

---
+++ thisisjava/src/ch01/versify/test.java
@@ -0,0 +1,11 @@
+package ch01.versify;
+
+public class test {
+	public static void main(String[] args) {
+		int x = 1, y  = 6;
+		while(y-- > 0) {
+			x++;
+		}
+		System.out.println(x + "d"+ y);
+	}
+}
